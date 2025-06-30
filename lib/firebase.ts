ae801fd67d933d78891a9fdffd668c70d9cd24a1
jiyoung-drow <shepollux@naver.com>
2025-06-30 10:17:34 +0900

    Rename: Modify Case

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,3 @@
-// trade/lib/firebase.ts
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
@@ -14,6 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
